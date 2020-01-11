6036d5187808eb8c77ee5c7d1f47e005fa1b4f16
Pankaj Bhardwaj <pankajbhj@gmail.com>
2020-01-11 16:50:36 +0530

    --Average calculate task for Adapt ready.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import First from './components/first'
-import Second from './components/second'
-import Welcome from './components/welcome'
-import Hello from './components/hello'
-import WelcomeVisitor from './components/welcomevisitor'
-import Counter from './components/counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from  './components/ParentComponent'
-import Namelist from  './components/Namelist'
-import PersonList from './components/Personlist'
-import LoginForm from './components/LoginForm'
+import Container from './container'
 
+
+// ParentComp for purecomponent
+//ClickCounter and HoverCounter for HOC demo
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* <ParentComponent />
-      <PersonList /> */}
-      <LoginForm />
-      {/* <Namelist /> */}
-      
-      {/* <EventBind /> */}
-      {/* <FunctionClick />
-      <ClassClick /> */}
-      {/* <Counter /> */}
-        {/* <WelcomeVisitor /> */}
-        {/* <First name = 'Pankaj' sirName = 'Bhardwaj'>
-          <p>This is children Props</p>
-        </First>
-        <First name = 'Tryambak' sirName = 'Dwivedi'>
-        <button>Action</button>
-        </First>
-        <First name = 'Subhi' sirName = 'Mishra'/>
-        <Welcome name = 'Pankaj' sirName = 'Bhardwaj'/> */}
-        {/* <Second />
-        <Hello /> */}
+      <Container/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import { Provider } from 'react-redux';
+import store from './components/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(    
+<Provider store={store}>
+    <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
